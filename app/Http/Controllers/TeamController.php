779763cc574c14dd64ecd7b332d7a03c506ed768
Franco Pacifico <weismurpowkaster@gmail.com>
2019-12-11 11:10:26 +0800

    the next next part

---
+++ app/Http/Controllers/TeamController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TeamController extends Controller
+{
+
+	public function index()
+	{
+		return view('teams');
+	}
+
+    protected function store(Request $request)
+    {
+        $this->validate($request, [
+            'teamname' => 'required',
+            'teamlogo' => 'required',
+        ]);
+        Video::create($request->all());
+        return redirect()->route('teams')
+                        ->with('success','Team created successfully');
+    }
